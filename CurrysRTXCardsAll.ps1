3e470ef41ff9c5a85e0a1efcde4e2409033feb26
Jovetic <jovetic@gmail.com>
2021-01-11 19:42:27 +0000

    Add files via upload

---
+++ CurrysRTXCardsAll.ps1
@@ -0,0 +1,10 @@
+﻿$CardList = Get-Content $PSScriptRoot\CardList.csv
+$pattern = 'FREE Delivery Available'
+
+Do{
+Foreach ($Card in $CardList){
+$cards = Invoke-RestMethod -Uri $Card
+Write-Host "Testing Card at "$card"" -ForegroundColor Yellow
+If ($cards -match $pattern) 
+{Start-Process $Card; Write-Host "Found a card on $(get-date) at"$Card"" -ForegroundColor Green }}
+}While ($cards -match $pattern -eq $False) 
