1394f43e6f5fa49cbc9b9ff377b7c60bd62c0819
Jovetic <jovetic@gmail.com>
2021-01-13 20:10:16 +0000

    Update CurrysRTXCardsAll.ps1
     Added timeout from suggesstion.

--- CurrysRTXCardsAll.ps1
+++ CurrysRTXCardsAll.ps1
@@ -3,7 +3,7 @@ $pattern = 'FREE Delivery Available'
 
 Do{
 Foreach ($Card in $CardList){
-$cards = Invoke-RestMethod -Uri $Card
+$cards = Invoke-RestMethod -Uri $Card -TimeoutSec 10
 Write-Host "Testing Card at "$card"" -ForegroundColor Yellow
 If ($cards -match $pattern) 
 {Start-Process $Card; Write-Host "Found a card on $(get-date) at"$Card"" -ForegroundColor Green }}
