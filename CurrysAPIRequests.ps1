e29cade52ca9e824245860672a00af7339091702
Jovetic <jovetic@gmail.com>
2021-01-13 20:33:57 +0000

    Added API Efforts
    Added my attempts at calling the API to collect information.  Seems limited on the payload back, but I'm new at this and not sure what paths to search

---
+++ CurrysAPIRequests.ps1
@@ -0,0 +1,20 @@
+﻿$ProductList = Get-Content $PSScriptRoot\ProductID.csv
+
+$Results = Foreach ($productid in $ProductList){
+
+    $Webrequest = Invoke-WebRequest -uri https://api.currys.co.uk/store/api/products/$productid
+    $InnerHTML = $WebRequest.ParsedHtml.body.OuterText | ConvertFrom-Json
+        
+
+                        [PSCustomObject] @{
+                                           ProductID       = $productid
+                                           ProductName     = $InnerHTML.payload.Label
+                                           ProductLink     = $InnerHTML.payload.Link
+                                           ProductPrice    = ($InnerHTML.payload.price.amount) /100
+                                           Category        = (($InnerHTML.payload.categorisation).segmentid)
+                                           HomeDelivery    = (($InnerHTML.payload.deliveryoptions | ?{$_.id -eq 1}).enabled)
+                                                     }
+
+                            }
+
+$Results | ft -AutoSize
